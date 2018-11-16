06b93cd46c08406f2732ea7c4c514b18d5209831
Trevlabz <trevlabz@email.arizona.edu>
2018-11-16 16:38:28 -0700

    created cmake project

---
+++ hlsyn/hlsyn/hlsyn.cpp
@@ -0,0 +1,15 @@
+﻿// hlsyn.cpp : Defines the entry point for the application.
+//
+
+#include "hlsyn.h"
+
+using namespace std;
+
+int main(){
+	string a;
+	a = "did this work?";
+	cout << a << endl;
+	cout << "Hello CMake." << endl;
+
+	return 0;
+}
